0dbb8b514d25feab82f52c7184e586fd2866f053
Erick <erickjhorman@gmail.com>
2021-01-16 20:37:28 -0500

    First commit

---
+++ src/main/java/com/example/co/restapispring/RestApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.co.restapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/co/restapispring/RestApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.co.restapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
